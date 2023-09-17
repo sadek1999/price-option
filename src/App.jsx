59d376ce812ac835bc1cf5cc1f6fbe39dee8133c
Sadek Ali <sadekali1530@gmail.com>
2023-09-17 21:20:00 +0600

    add the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Navbar from './component/navbar/navbar'
 
 function App() {
   
   return (
     <>
-      
+      <Navbar></Navbar>
       <h1>Vite + React</h1>
       
       
